e540b52e3be9f4ce4e5b17933e242d1f5d0db3df
busrauman <busrauman93@gmail.com>
2018-05-04 01:28:49 +0300

    Seperated Matrix additional functions that called MatrixUtils

--- MatrixTest.py
+++ MatrixTest.py
@@ -1,9 +1,19 @@
 from Matix import *
+from MatrixUtils import *
 
-
-A = Matrix(3,3)
+A = Matrix(2,2)
+B = Matrix(2,2)
+B.setMatrix()
 A.setMatrix()
 A.setItem(0,0,5)
 A.setItem(0,1,6)
 A.showMatrix()
-print(A.determinant(A.rows))
+B.showMatrix()
+trace(A.rows)
+#print(A.matrixMultiplication(B.rows))
+print(matrixMultiplication(A.rows,B.rows))
+
+print("trace : %d" % trace(A.rows))
+print(determinant(A.rows))
+
+print(scalarMultiplication(A.rows,3))
---
+++ MatrixUtils.py
@@ -0,0 +1,68 @@
+def trace(arg0):
+    trace = 0
+    if len(arg0) == len(arg0[0]):
+        for k in range(len(arg0)):
+            trace += arg0[k][k]
+    else:
+        print("Lütfen bir kare matris giriniz")
+    return trace
+
+
+def transpoze(arg0):
+    transpoze = [[0] * self.n for x in range(self.m)]
+    if len(arg0) == len(arg0[0]):
+        for i in range(len(arg0)):
+            for j in range(len(arg0[0])):
+                transpoze[j][i] = arg0[i][j]
+    else:
+        print("Lütfen kare bir matris giriniz")
+    return transpoze
+
+
+#eşit boyuttaki matrisler için çalışıyor düzenle burayı
+def matrixMultiplication(arg0, arg1):
+    result = [[0] * len(arg0) for x in range(len(arg1[0]))]
+    if len(arg0[0]) != len(arg1):
+        print("Birinci matrisin satır sayısı  ile ikinci matrisin sütün sayısı eşit olmalıdır ")
+    else:
+        for i in range(len(arg0)):
+            for j in range(len(arg0[0])):
+                for k in range(len(arg0[0])):
+                    result[i][j] += arg0[i][k] * arg1[k][j]
+    return result
+
+
+def determinant(matrix):
+    determinant = 0
+    if len(matrix) == 2:
+        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+    for i in range(len(matrix)):
+        pow = ((-1) ** i)
+        item = matrix[0][i]
+        m = coFactor(0, i, matrix)
+        determinant += pow * item * determinant(m)
+    return determinant
+
+
+def coFactor(self, i, j, matrix):
+    newMatrix = [[0] * (len(matrix) - 1) for x in range(len(matrix[0]) - 1)]
+    row = 0
+    col = 0
+    for x in range(len(matrix)):
+        if x != i:
+            for y in range(len(matrix[0])):
+                if x != i and y != j:
+                    newMatrix[row][col] = matrix[x][y]
+                    col += 1
+        if col >= (self.n - 1):
+            row += 1
+        col = 0
+    return newMatrix
+
+
+def scalarMultiplication(matrix,multiplier):
+     for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            matrix[i][j] *= multiplier
+
+     return matrix
